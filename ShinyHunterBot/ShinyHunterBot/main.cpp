c052e1f3e19b045aae01fcd13b2af91f86990215
Edi Gavrilovici <edi.gavrilovici@pixeldata.ro>
2020-09-30 18:19:02 +0300

    added qt core.

--- ShinyHunterBot/ShinyHunterBot/main.cpp
+++ ShinyHunterBot/ShinyHunterBot/main.cpp
@@ -1,5 +1,7 @@
+#include <QtCore/QCoreApplication>
 
-int main()
+int main(int argc, char* argv[])
 {
-	return 0;
+	QCoreApplication application(argc, argv);
+	return application.exec();
 }
---
+++ ShinyHunterBot/ShinyHunterBot/timeprocessing.cpp
@@ -0,0 +1,6 @@
+#include "timeprocessing.h"
+
+void shiny_hunter::TimeProcessing::CalibrateResetTime()
+{
+	
+}
